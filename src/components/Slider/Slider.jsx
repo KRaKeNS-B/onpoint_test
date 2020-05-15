6a0637e4ffc41266eefb2fbbdd2b718a7fee72fe
Max Krak <krakenslab@gmail.com>
2020-05-16 01:50:44 +0300

    events handling added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import styles from './Slider.module.css'
 import Slide1 from '../Slide1/Slide1'
 import Slide2 from '../Slide2/Slide2'
@@ -6,12 +6,12 @@ import Slide3 from '../Slide3/Slide3'
 
 const Slider = props => {
   const [activeVerticalSlide, setActiveVerticalSlide] = useState(1)
-  const maxNumVSlides = 3
-  let pagination = []
+  const [scrollingIsActive, setScrollingIsActive] = useState(false)
+  const [touchStartY, setTouchStartY] = useState(0)
 
-  const handleScroll = () => {
-    console.log('scroll')
-  }
+  const maxNumVSlides = 3
+  const scrollingTime = 800
+  let pagination = []
 
   const getSliderInnerStyle = (numSlides, currentSlide) => {
     const percent = -100 * (currentSlide - 1) / numSlides
@@ -23,20 +23,76 @@ const Slider = props => {
 
   const [sliderInnerStyle, setSliderInnerStyle] = useState(getSliderInnerStyle(maxNumVSlides, 1))
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
+  const changePage = (currentElement) => {
+    setSliderInnerStyle(getSliderInnerStyle(maxNumVSlides, currentElement))
+    setActiveVerticalSlide(currentElement)
+  }
+
+  const wheelTimeout = () => {
+    setScrollingIsActive(true)
+    setTimeout(
+      () => {
+        return setScrollingIsActive(false);
+      },
+      scrollingTime
+    )
+  }
+
+  const scrollingUp = () => {
+    if (activeVerticalSlide + 1 <= maxNumVSlides) {
+      changePage(activeVerticalSlide + 1)
+      wheelTimeout()
     }
-  })
+  }
+
+  const scrollingDown = () => {
+    if (activeVerticalSlide - 1 > 0) {
+      changePage(activeVerticalSlide - 1)
+      wheelTimeout()
+    }
+  }
+
+  const mouseWheelEvent = (e) => {
+    if (scrollingIsActive) {
+      return
+    }
+
+    if (e.deltaY > 0 || e.detail < 0) {
+      scrollingUp()
+    }
+    else if (e.deltaY < 0 || e.detail > 0) {
+      scrollingDown()
+    }
+  }
+
+  const handleTouchStart = (e) => {
+    if (scrollingIsActive) {
+      return;
+    }
+
+    setTouchStartY(e.targetTouches[0].clientY)
+  }
+
+  const handleTouchMove = (e) => {
+    if (scrollingIsActive) {
+      return;
+    }
+
+    const touchEndX = e.targetTouches[0].clientY
+
+    if (touchStartY - touchEndX < -200) {
+      scrollingDown()
+    } else if (touchStartY - touchEndX > 200) {
+      scrollingUp()
+    }
+  }
 
   // https://codepen.io/tari/pen/KzzzwX - css slider
   // https://codepen.io/WispProxy/pen/EyLWKg
   // https://codepen.io/supah/pen/zZaPeE - events
 
-  const onPaginationClick = (activeElement, event) => {
-    setSliderInnerStyle(getSliderInnerStyle(maxNumVSlides, activeElement))
-    setActiveVerticalSlide(activeElement)
+  const onPaginationClick = (currentElement, event) => {
+    changePage(currentElement)
   }
 
   for (let index = 1; index <= maxNumVSlides; index++) {
@@ -54,7 +110,9 @@ const Slider = props => {
       <div
         className={`${styles.mainContainer} ${styles.slider__inner}`}
         style={sliderInnerStyle}
-        onScroll={handleScroll}
+        onWheel={mouseWheelEvent}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
       >
         <Slide1 className={styles.slider__contents} />
         <Slide2 className={styles.slider__contents} />
